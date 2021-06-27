372f30081414a255f6b072926c20a4057c428628
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-06-26 17:23:43 -0700

    Update script.lua

--- licensechanger/script.lua
+++ licensechanger/script.lua
@@ -1,1 +1,29 @@
+local modulescript = game:GetService("ServerScriptService"):WaitForChild("LicenseTable")
+local licenses = require(modulescript)
 
+function updatelicense(player, text)
+	local player = game.Players:FindFirstChild(text)
+	if player ~= nil then
+		table.insert(licenses, text)
+	else
+		print("Invalid Input!!!!")
+	end
+end
+
+function removeplayer(player, text)
+	local player = game.Players:FindFirstChild(text)
+	if player ~= nil then
+		local count = 0
+		local removefromplayer = table.find(licenses, text)
+		if removefromplayer ~= nil then
+			licenses[removefromplayer] = nil
+		else
+			print("Invalid Input!!!!")
+		end
+	else
+		print("Invalid Input!!!!")
+	end
+end
+
+script.Parent.Giver.OnServerEvent:Connect(updatelicense)
+script.Parent.Remover.OnServerEvent:Connect(removeplayer)
